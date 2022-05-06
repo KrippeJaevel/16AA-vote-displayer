d0dca714d5be4b5090e538c46a76cf9a9f19a372
KrippeJaevel <93353088+KrippeJaevel@users.noreply.github.com>
2022-05-06 19:25:44 +0200

    Update vote.py
    updated to source of version 1.2 of the vote script

--- vote.py
+++ vote.py
@@ -1,24 +1,46 @@
+from datetime import date
 from bs4 import BeautifulSoup
 import requests
-import sys
 import webbrowser
-
-# When called via cmd the user needs to specify the URL to the event to check, this should be automated if run serverside
-if len(sys.argv) < 2:
-    print("You need to invoke this script along with an URL to the event you want to check. Example below:")
-    print("vote.py https://community.16aa.net/calendar/event/582-operation-hidden-trigger-xi/\n")
-
-
+import sys
 OrbatURL = "https://community.16aa.net/orbat/"
-attendanceURL = sys.argv[1]
 # When running serverside: replace the above attendanceURL with one of the below (not sure which one due to not being on a server yet)
-#attendanceURL = self.request.url
-# OR:
-#attendanceURL = self.request.query_string
+#ALT1 attendanceURL = self.request.url
+#ALT2 attendanceURL = self.request.query_string
 
 # Locates the orbat and parses the content
 orbatPage = requests.get(OrbatURL)
 orbat = BeautifulSoup(orbatPage.content, "html.parser")
+
+def get_attendance_URL():
+    attendanceURL = input("Please enter the URL to the event you want to check:\n")
+    if attendanceURL.find("https://community.16aa.net/calendar/event/") < 0:
+        print("Not like that...\nIt should look something like this:\nhttps://community.16aa.net/calendar/event/666-operation-gather-votes-xiv/")
+        attendanceURL = get_attendance_URL()
+    return attendanceURL
+
+attendanceURL = get_attendance_URL()
+calendarURL = "https://community.16aa.net/calendar/"
+calendarRAW = requests.get(calendarURL)
+calendar = BeautifulSoup(calendarRAW.content, "html.parser")
+
+
+# Finds today. Navigates to last day in current week. Grabs the Sunday - 20.00 one (UK time)
+"""def get_next_event():
+    based_of_today = calendar.find("td", {"class": "cCalendar_date cCalendar_today"})
+    week_view = based_of_today.find_parent("tr")
+    list_of_events = week_view.find_all("li")[-1]
+    sunday_op = ""
+    for tag in list_of_events:
+        index = str(tag).find("href")
+        index_last = index
+        if index > 0 and str(tag).find('time">08:00  PM') > 0:
+            index_last =  str(tag).find(" ",index)
+            sunday_op = str(tag)[index+6:index_last-1]
+            break
+    return sunday_op"""
+
+#attendanceURL = get_next_event()
 
 
 # Parameter section is the id of the div displaying the members on the ORBAT ("x/y section" or similar)
@@ -35,12 +57,17 @@ def get_section_members(section):
 def get_voters():
     attendancePage = requests.get(attendanceURL)
     attendance = BeautifulSoup(attendancePage.content, "html.parser")
-    full_selection_voted_yes = attendance.find("div", id="ipsTabs_elAttendeesMob_elGoing_panel")
-    full_selection_voted_maybe = attendance.find("div", id="ipsTabs_elAttendeesMob_elMaybe_panel")
-    full_selection_voted_no = attendance.find("div", id="ipsTabs_elAttendeesMob_elNotGoing_panel")
-    attending_voters = full_selection_voted_yes.find_all("a")
-    maybe_voters = full_selection_voted_maybe.find_all("a")
-    loa_voters = full_selection_voted_no.find_all("a")
+    try:
+        full_selection_voted_yes = attendance.find("div", id="ipsTabs_elAttendeesMob_elGoing_panel")
+        full_selection_voted_maybe = attendance.find("div", id="ipsTabs_elAttendeesMob_elMaybe_panel")
+        full_selection_voted_no = attendance.find("div", id="ipsTabs_elAttendeesMob_elNotGoing_panel")
+        attending_voters = full_selection_voted_yes.find_all("a")
+        maybe_voters = full_selection_voted_maybe.find_all("a")
+        loa_voters = full_selection_voted_no.find_all("a")
+    except AttributeError as error:
+        print("Make sure your URL was correct. I couldn't find anything at all there...")
+        input("This will turn off now. Press any key and restart. Do it right this time. Bye!\n")
+        sys.exit()
     votes_dictionary = {"yes": [], "maybe": [], "no": []}
     for voter in attending_voters:
         votes_dictionary["yes"].append(voter.text[:-2].strip())
@@ -93,8 +120,7 @@ def render_page(section_dictionary):
     return page
 
 # Get all the members from each section tagged with a unique ID on the ORBAT
-# Coy HQ is not tagged with an ID (assumed to be "Coy HQ"), therefore commented out
-#coyhq =  get_section_members("Coy HQ")
+coyhq =  get_section_members("Coy HQ")
 oneplt = get_section_members("1 Platoon HQ")
 oneone = get_section_members("1/1 Section")
 onetwo = get_section_members("1/2 Section")
@@ -106,12 +132,13 @@ fsg = get_section_members("FSG")
 aasr = get_section_members("13AASR")
 csmr = get_section_members("16CSMR")
 jhc = get_section_members("JHC")
-# FST is tagged twice, with all of Attachments being the first.
-#fst = get_section_members("FST")
+fst = get_section_members("JFIST")
+sig = get_section_members("216 Sigs")
 pathfinders = get_section_members("Pathfinders")
+mi = get_section_members("3/2 Section")
 
 # Compile a list of the company and a variable to hold the page
-the_unit = [oneplt, onetwo, oneone,  onethree, twoplt, twotwo, twothree, fsg, aasr, csmr, jhc,pathfinders]
+the_unit = [coyhq, oneplt, oneone, onetwo, onethree, twoplt, twotwo, twothree, fsg, aasr, csmr, jhc, fst, sig, pathfinders, mi]
 page = ""
 
 # Iterate through the list of the company, rendering the HTML-template for each of them  
@@ -120,7 +147,8 @@ for section in the_unit:
     page += render_page(current_section)
 
 # Open the target HTML-file, permissions to write to it, encoding utf-8 to ensure unicode support
-f = open("attendance.html", "w",encoding="utf-8")
+filename = "{}.html".format(attendanceURL[42:-1])
+f = open(filename, "w",encoding="utf-8")
 f.write(page)
 f.close()
-webbrowser.open_new_tab("attendance.html")
+webbrowser.open_new_tab(filename)
